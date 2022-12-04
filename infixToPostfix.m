8149b428a75253ff1ffce0a000e64991787dc7df
AleczSC <jesusa.soto@potros.itson.edu.mx>
2022-12-04 15:31:34 -0700

    vector postfix actualizado

--- infixToPostfix.m
+++ infixToPostfix.m
@@ -1,20 +1,20 @@
 function postfix = infixToPostfix(cadena)
     operadores = java.util.Stack(); %Stack para guardar operadores
-    postfix = java.util.ArrayList();
+    postfix = [];
     
     lista = split(cadena);          %Separar los elementos 
     for i = 1:length(lista)
         if ~isstrprop(lista{i}, 'alphanum')
           while (~operadores.isEmpty() && (menorPrecedencia(lista{i}, operadores.peek())))
-              postfix.add(operadores.pop())
+              postfix = [postfix operadores.pop()];
           end
           operadores.add(lista{i});
         else
-             postfix.add(lista{i});
+              postfix = [postfix lista{i}];
         end   
     end
     while(~operadores.empty)
-         postfix.add(operadores.pop());
+         postfix = [postfix operadores.pop()];
     end
 end
 
