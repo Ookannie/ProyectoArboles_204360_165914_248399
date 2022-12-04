84c4def1cf49196250dfda7d5659a52c0060a57e
AleczSC <jesusa.soto@potros.itson.edu.mx>
2022-12-04 12:00:31 -0700

    Create infixToPostfix.m
    Subida de archivo

---
+++ infixToPostfix.m
@@ -0,0 +1,13 @@
+function postfix = infixToPostfix(cadena)
+    operadores = java.util.Stack(); %Stack para guardar operadores
+
+    lista = split(cadena);          %Separar los elementos 
+    
+    for i = 1:length(lista)
+        if isstrprop(cadena, 'alphanum')
+          while (~operadores.isempty && ) 
+       end
+       
+    end
+    
+end
