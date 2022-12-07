ae83d917c2b36216d4438743024ea4716026bbda
Ana Gamez <ana.gamez.elizalde@gmail.com>
2022-12-07 14:30:20 -0700

    tree

--- createExpressionTree.m
+++ createExpressionTree.m
@@ -5,18 +5,19 @@ function expressionTree = createExpressionTree(postfixExpression)
     for i = 1:length(postfix)
         if(~isOperator(postfix(i)))
             operand = Nodo(postfix(i));
-            nodes(end+1) = postfix(i);
+            nodes = [nodes operand];
         else
             operator = Nodo(postfix(i));
             
             operandLeft = nodes(end);
-            nodes(end) = []
+            operator.leftNode = operandLeft;
+            nodes(end) = [];
+            
             
             operandRight = nodes(end);
-            nodes(end) = []
-            
-            operator.leftNode = operandLeft;
             operator.rightNode = operandRight;
+            nodes(end) = [];
+            
             
            nodes = [nodes operator];
         end
