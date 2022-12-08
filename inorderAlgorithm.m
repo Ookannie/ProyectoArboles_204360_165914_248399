a32641ebac8f207a7d5930823d24c8526687e2b2
Ana Gamez <ana.gamez.elizalde@gmail.com>
2022-12-07 21:33:52 -0700

    evaluacion de la expresion matematica, cambio de nombre de algoritmo inorder

--- inorderAlgorithm.m
+++
@@ -1,9 +0,0 @@
-function inorder = inorderAlgorithm(node)
-    if(isempty(node))
-        return
-    end
-    
-    inorderAlgorithm(node.leftNode);
-    inorder = node.value
-    inorderAlgorithm(node.rightNode);
-end
---
+++ inorderAlgorithmEval.m
@@ -0,0 +1,34 @@
+function result = inorderAlgorithmEval(node)    
+    if(isempty(node))
+        result = 0;
+        return
+    end
+    
+    if(isempty(node.leftNode) && isempty(node.rightNode))
+        result = str2num(node.value);
+        return
+    end
+   
+    resultLeft = inorderAlgorithmEval(node.leftNode);
+    resultRight = inorderAlgorithmEval(node.rightNode);
+    
+    result = evaluateOperation(node.value, resultLeft, resultRight);
+    return
+end
+
+function result = evaluateOperation(operator, leftValue, rightValue)
+    switch operator
+        case {'+'}
+            result = leftValue + rightValue;
+        case {'-'}
+            result = leftValue - rightValue;
+        case {'/'}
+            result = leftValue / rightValue;
+        case {'*'}
+            result = leftValue * rightValue;
+        case {'\','%'}
+            result = mod(leftValue, rightValue);
+        case {'^'}
+            result = leftValue ^ rightValue;
+    end
+end
